bcb6eace399df7883c8a07b986d0559b44fab251
Joseph Guest <JoeGuest@Josephs-MacBook-Air.local>
2014-10-22 14:09:48 +0100

    change structure between Player and Game, Player can make guess, Answer is updated
    We previously used Player.start_game, but have now redesigned so that we pass
    in both the Player and Answer to a Game object. This is avoid complications with
    multiple players -- Player1.start_game, Player2.start_game -- would result in
    different game.
    
    We can make guesses through the Player now via Player.make_guess("hello").
    
    This will instantiate a new Guess object, pass to Game, which will then pass
    to Answer, which will update the Answer.current_answer and fill in blanks.
    
    The Game object in this sense is a kind of handler for our other objects. We
    will suffer the consequences of this when it's too late.

--- features/step_definitions/single_player_game_steps.rb
+++ features/step_definitions/single_player_game_steps.rb
@@ -5,23 +5,26 @@ Given(/^a new player$/) do
 end
 
 When(/^player starts a new game$/) do
-  @player.start_game
+  @answer = Answer.new("Help")
+  @game = Game.new(@player, @answer)  
 end
 
 Then(/^new game is generated$/) do
-  expect(@player.game).to be_a Game
+  expect(@player.current_game).to be_a Game
 end
 
 Given(/^an incomplete word$/) do
-  pending # express the regexp above with the code you wish you had
+  @player = Player.new
+  @answer = Answer.new("Help")
+  Game.new(@player, @answer)
 end
 
 When(/^player makes a correct guess$/) do
-  pending # express the regexp above with the code you wish you had
+  @player.make_guess "H"
 end
 
 Then(/^fill in blank\(s\) with letter$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@answer.current_answer).to eq ["H", "_", "_", "_"]
 end
 
 When(/^player makes an incorrect guess$/) do
--- features/support/scratchpad.rb
+++ features/support/scratchpad.rb
@@ -1,18 +1,58 @@
 class Player
-  attr_reader :game
+  attr_accessor :current_game
 
-  def start_game
-    @game = Game.new(self)
+  def make_guess(letter)
+    guess = Guess.new(letter)
+    current_game.check_guess(guess)
   end
 end
 
 class Game
-  def initialize(player)
+  def initialize(player, answer)
+    @player = player
+    player.current_game = self
+
+    @answer = answer
+  end
+
+  def check_guess(guess)
+    if @answer.guess!(guess)
+      @answer.current_answer
+    else
+      # error
+    end
   end
 end
 
 class Guess
+  attr_reader :letter
+
+  def initialize(letter)
+    @letter = letter
+  end
 end
 
 class Answer
+  attr_reader :word, :current_answer
+
+  def initialize(word)
+    # generate answer from dictionary
+    @word = word || "Test"
+    @current_answer = all_blanks
+  end
+
+  def guess!(guess)
+    # return true if correct, else false
+    word.split("").each_with_index do |letter, index|
+      if guess.letter == letter
+        @current_answer[index] = letter
+      end
+    end
+  end
+
+  private
+
+  def all_blanks
+    ["_"] * word.length
+  end
 end
