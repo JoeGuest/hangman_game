90710fd1fd28531b92388ee7efb69d1aac7a668a
Joseph Guest <JoeGuest@Josephs-MacBook-Air.local>
2014-10-23 10:47:25 +0100

    integrate scratchpad with sinatra app, works from frontend

--- app/hangman.rb
+++ app/hangman.rb
@@ -1,10 +1,21 @@
 require "sinatra"
+require "sinatra/reloader"
+
+require "./features/support/scratchpad.rb"
 
 get "/" do
   erb :splash
 end
 
 get "/hangman" do
+
+  unless settings.respond_to?(:game)
+    player = Player.new
+    answer = Answer.new("Avengers")
+    game = Game.new(player, answer)
+    Sinatra::Application.set :game, game
+  end
+
   erb :game, locals: { game: settings.game }
 end
 
