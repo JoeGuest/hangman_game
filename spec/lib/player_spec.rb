6fbffc159911fa179fda3c345bf46d34696e8899
Joseph Guest <JoeGuest@Josephs-MacBook-Air.local>
2014-10-27 10:55:54 +0000

    spec player class, pre-empting refactor of make_guess

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -7,17 +7,32 @@ describe Player do
     expect(player.lives).to eq 10
   end
 
-  it "allows you to set lives"
+  it "allows you to set lives" do
+    player = Player.new(5)
+    expect(player.lives).to eq 5
+  end
 
   describe "#make_guess" do
-    it "creates a Guess object and sends to Game"
+    it "creates a Guess object and sends to Game" do
+
+    end
   end
 
   describe "#lose_life!" do
-    it "decrements lives"
+    it "decrements lives" do 
+      player.lose_life!
+      expect(player.lives).to eq 9
+    end
   end
 
   describe "#dead?" do
-    it "returns status of player"
+    it "returns true when player has no lives" do
+      player = Player.new(0)
+      expect(player.dead?).to eq true
+    end
+
+    it "returns false if player has lives" do
+      expect(player.dead?).to eq false
+    end
   end
 end
