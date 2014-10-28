865eddb3ff8d179f9318d767fa984ed296f5557e
Joseph Guest <JoeGuest@Josephs-MacBook-Air.local>
2014-10-28 11:15:49 +0000

    add specs for base_score on Abacus

--- spec/lib/abacus_spec.rb
+++ spec/lib/abacus_spec.rb
@@ -14,6 +14,54 @@ describe Abacus do
     end
   end
 
+
+  %w(g d).each do |letter|
+    it "returns 20 for #{letter}" do
+      guess = Guess.new(letter)
+      expect(Abacus.new(guess, answer).base_score).to eq 20
+    end
+  end
+
+
+  %w(b c m p).each do |letter|
+    it "returns 30 for #{letter}" do
+      guess = Guess.new(letter)
+      expect(Abacus.new(guess, answer).base_score).to eq 30
+    end
+  end
+
+
+  %w(y f v w).each do |letter|
+    it "returns 40 for #{letter}" do
+      guess = Guess.new(letter)
+      expect(Abacus.new(guess, answer).base_score).to eq 40
+    end
+  end
+
+
+  %w(k).each do |letter|
+    it "returns 50 for #{letter}" do
+      guess = Guess.new(letter)
+      expect(Abacus.new(guess, answer).base_score).to eq 50
+    end
+  end
+
+
+  %w(j x).each do |letter|
+    it "returns 80 for #{letter}" do
+      guess = Guess.new(letter)
+      expect(Abacus.new(guess, answer).base_score).to eq 80
+    end
+  end
+
+
+  %w(q z).each do |letter|
+    it "returns 100 for #{letter}" do
+      guess = Guess.new(letter)
+      expect(Abacus.new(guess, answer).base_score).to eq 100
+    end
+  end
+
   context "for correct guesses"
   context "for streaks"
 
